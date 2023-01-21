6bfb3c9afcea183f99597da47966ce857a90cee2
Clara Charriau <charriau.clara@gmail.com>
2023-01-21 11:31:42 +0100

    update work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/general/header/header.component';
+import { FooterComponent } from './components/general/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LandingComponent } from './components/homepage/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
